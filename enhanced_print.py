e0c3bd12de397ea311f94d5f4512b0e027a32728
Yunchuan-Xu <alanxu428@gmail.com>
2018-04-04 10:56:36 +0800

    Update enhanced_print.py

--- enhanced_print.py
+++ enhanced_print.py
@@ -23,14 +23,13 @@ def show_content(obj, name='root', **kwargs):
         print("{}{}{}".format(_kwargs['padding'], _kwargs['padding_tail'], name))
         _kwargs['padding'] = _kwargs['padding'] + _kwargs['padding_following']
         for i, item in enumerate(obj):
-            item_name = "[{}]".format(i) if isinstance(obj, list) else item
+            item, item_name = (item, "[{}]".format(i)) if isinstance(obj, list) else (obj[item], item)
             if i < len(obj) - 1:
                 _kwargs['padding_tail'] = _kwargs['padding_child_birth']
                 _kwargs['padding_following'] = _kwargs['padding_child_alive']
             else:
                 _kwargs['padding_tail'] = _kwargs['padding_last_child_birth']
                 _kwargs['padding_following'] = _kwargs['padding_last_child_alive']
-            item = item if isinstance(obj, list) else obj[item]
             show_content(item, item_name, **_kwargs)
     else:
         print("{}{}{}: {}".format(_kwargs['padding'], _kwargs['padding_tail'], name, obj))
